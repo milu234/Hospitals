e865e20d17f5affbad091a96c952defe50adb1d8
milu234 <milanhazra234.mh@gmail.com>
2019-09-21 08:57:40 +0530

    JSON Data Parsing

--- app/src/main/java/com/example/hospitals/DataParser.java
+++ app/src/main/java/com/example/hospitals/DataParser.java
@@ -1,13 +1,16 @@
 package com.example.hospitals;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DataParser
 {
-    private HashMap<String,String> getPlace(JSONObject googlePlaceJSON){
+    private HashMap<String,String> getSinglePlace(JSONObject googlePlaceJSON){
         HashMap<String,String> googlePlaceMap = new HashMap<>();
         String NameOfPlace = "-NA-";
         String vicinity = "-NA-";
@@ -46,5 +49,45 @@ public class DataParser
         }
 
         return googlePlaceMap;
+    }
+
+    private List<HashMap<String,String>> getAllNearbyPlaces(JSONArray jsonArray)
+    {
+        int counter = jsonArray.length();
+        List<HashMap<String,String>> NearbyPlacesList = new ArrayList<>();
+
+        HashMap <String,String> NearbyplaceMap = null;
+
+        for (int i = 0 ; i<counter;i++)
+        {
+            try
+            {
+                NearbyplaceMap = getSinglePlace((JSONObject) jsonArray.get(i));
+                NearbyPlacesList.add(NearbyplaceMap);
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return NearbyPlacesList;
+    }
+
+
+    public List<HashMap<String,String>> parse(String jsonData)
+    {
+        JSONArray jsonArray = null;
+        JSONObject jsonObject;
+
+        try
+        {
+            jsonObject = new JSONObject(jsonData);
+            jsonArray = jsonObject.getJSONArray("results");
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+    return getAllNearbyPlaces(jsonArray);
     }
 }
